6ed0d5a17e31bea6a84e90601b84f42640aea154
Wilhelm Drehling <wilhelmdrehling@web.de>
2022-01-19 16:51:07 +0100

    Update SmallRSA.py
    Small Changes

--- SmallRSA.py
+++ SmallRSA.py
@@ -10,16 +10,14 @@ phi = (p - 1) * (q - 1)
 # Schlüsselgenerierung
 e = 11
 d = inverse(e, phi)
-print("Privater Schlüssel: ({:d},{:d})".format(e, n))
-print("Öffentlicher Schlüssel: ({:d},{:d})".format(d, n))
+print("Öffentlicher Schlüssel: ({:d},{:d})".format(e, n))
+print("Privater Schlüssel: ({:d},{:d})".format(d, n))
 
 # Nachricht
 nachricht = int(input("Beliebige Zahl zwischen 0 und 221 eintippen: "))
 
 # Verschlüsseln
 c = pow(nachricht, e, n)
-print("Verschlüsselt: " + str(c))
 
 # Entschlüsseln
-m = pow(c, d, n)
-print("Entschlüsselt: " + str(m))
+print(str(pow(cypher, d, n)))
